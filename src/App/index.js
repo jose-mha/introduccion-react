66610b8eee4624635bbe997a8b78f063f31d5ca4
José Manuel Hernández Antaño <jose.hernandez@gonet.us>
2021-12-07 17:20:16 -0600

    :sparkles: Persistencia de datos con Local Storage
    Uso del local storage para la persistencia de datos y no se pierdan al recargar la pagina

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import AppUI from './AppUI';
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: false },
-  { text: 'Tomar el curso de intro a react', completed: true },
-  { text: 'Aprender Nodejs', completed: true },
-];
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: false },
+//   { text: 'Tomar el curso de intro a react', completed: true },
+//   { text: 'Aprender Nodejs', completed: true },
+// ];
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
@@ -27,12 +37,18 @@ function App() {
     });
   }
 
+  const saveTodos = (newTodos) => {
+    const stringifyTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_v1', stringifyTodos);
+    setTodos(newTodos);
+  };
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
 
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
@@ -40,7 +56,7 @@ function App() {
 
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
