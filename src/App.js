064edaf3bc1f782240d24ac5adac35551a809099
José Manuel Hernández Antaño <jose.hernandez@gonet.us>
2021-12-07 15:53:12 -0600

    :zap: Buscando Todos y contando
    Usando filter y usestate

--- src/App.js
+++ src/App.js
@@ -6,20 +6,39 @@ import TodoItem from './TodoItem';
 import CreateTodoButton from './CreateTodoButton';
 // import './App.css';
 
-const todos = [
+const defaultTodos = [
   { text: 'Cortar cebolla', completed: false },
   { text: 'Tomar el curso de intro a react', completed: true },
-  { text: 'Aprender Nodejs', completed: false },
+  { text: 'Aprender Nodejs', completed: true },
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+
+      return todoText.includes(searchText);
+    });
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter total={totalTodos} completed={completedTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {todos.map((todo) => (
+        {searchedTodos.map((todo) => (
           <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
         ))}
       </TodoList>
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
-  const [searchValue, setSearchValue] = React.useState('');
+function TodoSearch({ searchValue, setSearchValue }) {
+  const [] = React.useState('');
 
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
     setSearchValue(event.target.value);
   };
 
-  return [<input className="TodoSearch" placeholder="Cebolla" value={searchValue} onChange={onSearchValueChange} />, <p>{searchValue}</p>];
+  return <input className="TodoSearch" placeholder="Cebolla" value={searchValue} onChange={onSearchValueChange} />;
 }
 
 export default TodoSearch;
