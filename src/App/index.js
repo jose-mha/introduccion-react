37b86de49c7afa941bbf3125323bdf7b216c6335
José Manuel Hernández Antaño <jose.hernandez@gonet.us>
2021-12-07 16:30:26 -0600

    :art: Organizacion de archivos y carpetas
    Se organizaron las carpetas y los archivos para una mejor comprension

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import TodoCounter from './TodoCounter';
-import TodoSearch from './TodoSearch';
-import TodoList from './TodoList';
-import TodoItem from './TodoItem';
-import CreateTodoButton from './CreateTodoButton';
-// import './App.css';
+import AppUI from './AppUI';
 
 const defaultTodos = [
   { text: 'Cortar cebolla', completed: false },
@@ -49,24 +44,15 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <TodoCounter total={totalTodos} completed={completedTodos} />
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-
-      <TodoList>
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-    </React.Fragment>
+    <AppUI
+      total={totalTodos}
+      completed={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
