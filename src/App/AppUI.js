403041a01997571b372edb1c8c933a5ac17f3c07
José Manuel Hernández Antaño <jose.hernandez@gonet.us>
2021-12-07 21:09:00 -0600

    :sparkles: Portales
    Hacemos el uso de los portales(Modales)

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -5,33 +5,38 @@ import TodoSearch from '../TodoSearch';
 import TodoList from '../TodoList';
 import TodoItem from '../TodoItem';
 import CreateTodoButton from '../CreateTodoButton';
+import { Modal } from '../Modal';
 
 function AppUI() {
+  const { error, loading, searchedTodos, completeTodo, deleteTodo, openModal, setOpenModal } = React.useContext(TodoContext);
+
   return (
     <React.Fragment>
       <TodoCounter />
       <TodoSearch />
 
-      <TodoContext.Consumer>
-        {({ error, loading, searchedTodos, completeTodo, deleteTodo }) => (
-          <TodoList>
-            {error && <p>Desesperate, hubo un error...</p>}
-            {loading && <p>Estamos cargando......</p>}
-            {!loading && !searchedTodos.length && <p>Crea tu primer TODO</p>}
-            {searchedTodos.map((todo) => (
-              <TodoItem
-                key={todo.text}
-                text={todo.text}
-                completed={todo.completed}
-                onComplete={() => completeTodo(todo.text)}
-                onDelete={() => deleteTodo(todo.text)}
-              />
-            ))}
-          </TodoList>
-        )}
-      </TodoContext.Consumer>
+      <TodoList>
+        {error && <p>Desesperate, hubo un error...</p>}
+        {loading && <p>Estamos cargando......</p>}
+        {!loading && !searchedTodos.length && <p>Crea tu primer TODO</p>}
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
 
-      <CreateTodoButton />
+      {!!openModal && (
+        <Modal>
+          <p>Ejemplo del modal</p>
+        </Modal>
+      )}
+
+      <CreateTodoButton setOpenModal={setOpenModal} />
     </React.Fragment>
   );
 }
